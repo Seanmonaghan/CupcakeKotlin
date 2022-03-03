6ab75a8957bca2b06783751e77d954e77572c738
Sean Monaghan <smonagha@conncoll.edu>
2022-03-03 17:03:43 -0500

    added view model references to each relevant fragment

--- app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
+++ app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -3,26 +3,74 @@ package com.example.cupcake.model
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.text.SimpleDateFormat
+import java.util.*
+
+private const val PRICE_PER_CUPCAKE = 2.00
 
 class OrderViewModel: ViewModel() {
-    private val _quantity = MutableLiveData<Int>(0)
+
+//    Values and accessors
+    private val _quantity = MutableLiveData<Int>()
     val quantity: LiveData<Int> = _quantity
+
+    private val _flavor = MutableLiveData<String>()
+    val flavor: LiveData<String> = _flavor
+
+    private val _date = MutableLiveData<String>()
+    val date: LiveData<String> = _date
+
+    private val _price = MutableLiveData<Double>()
+    val price: LiveData<Double> = _price
+
+    val dateOptions = getPickupOptions()
+
+//    Customer setter functions
+
     fun setQuantity(numberCupcakes: Int) {
         _quantity.value = numberCupcakes
+        updatePrice()
     }
 
-    private val _flavor = MutableLiveData<String>("")
-    val flavor: LiveData<String> = _flavor
     fun setFlavor(desiredFlavor: String) {
         _flavor.value = desiredFlavor
     }
 
-    private val _date = MutableLiveData<String>("")
-    val date: LiveData<String> = _date
     fun setDate(pickupDate: String) {
         _date.value = pickupDate
     }
 
-    private val _price = MutableLiveData<Double>(0.0)
-    val price: LiveData<Double> = _price
+    private fun updatePrice() {
+        _price.value = (quantity.value ?: 0) * PRICE_PER_CUPCAKE
+    }
+
+//    Initialization Function
+
+    init {
+        resetOrder()
+    }
+
+//    Utility Functions
+
+    private fun getPickupOptions(): List<String> {
+        val options = mutableListOf<String>()
+        val formatter = SimpleDateFormat("E MMM d", Locale.getDefault())
+        val calendar = Calendar.getInstance()
+        repeat(4) {
+            options.add(formatter.format(calendar.time))
+            calendar.add(Calendar.DATE, 1)
+        }
+        return options
+    }
+
+    fun hasNoFlavorSet(): Boolean {
+        return _flavor.value.isNullOrEmpty()
+    }
+
+    fun resetOrder() {
+        _quantity.value = 0
+        _flavor.value = ""
+        _date.value = dateOptions[0]
+        _price.value = 0.0
+    }
 }
